a4e33fa79c126884e8c5de14713cd0b35a57de72
EC2 Default User <ec2-user@ip-172-31-3-59.ec2.internal>
2022-09-30 18:00:18 +0000

    [Start] Meshiterro

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to:  'homes#top'
+  get 'homes/about', to: 'homes#about', as: 'about'
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
